10c2a74131d2a39e58428065850dd1405854b197
Your Name <you@example.com>
2020-09-17 17:31:12 +0700

    https://stackoverflow.com/questions/35760176/generate-a-16-digit-unique-hexadecimal-value-from-given-string-of-numbers

--- app/src/main/java/com/example/prototype1/MainActivity.java
+++ app/src/main/java/com/example/prototype1/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.prototype1;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.answer1);
+        textView = findViewById(R.id.question1Header);
+
+        button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    toHash();
+                } catch (BadPaddingException e) {
+                    e.printStackTrace();
+                } catch (IllegalBlockSizeException e) {
+                    e.printStackTrace();
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (InvalidKeyException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void toHash() throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        String string = editText.getText().toString();
+        Log.v("EditText", string);
+        byte[] plaintext = string.getBytes();
+
+        Cipher c = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(new byte[16], "DESede"));
+        byte[] cipherText = c.doFinal(plaintext);
+        textView.setText(cipherText.toString());
     }
 }
