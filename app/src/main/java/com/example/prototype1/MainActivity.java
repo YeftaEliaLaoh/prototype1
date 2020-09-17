6c04c783247651cac07557c7e1abe6548d8de717
Your Name <you@example.com>
2020-09-17 20:22:40 +0700

    https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function

--- app/src/main/java/com/example/prototype1/MainActivity.java
+++ app/src/main/java/com/example/prototype1/MainActivity.java
@@ -17,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     Button button;
     EditText editText;
-    TextView textView;
+    TextView textView1;
+    TextView textView2;
+
     String string;
 
     @Override
@@ -26,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = findViewById(R.id.answer1);
-        textView = findViewById(R.id.question1Header);
+        textView1 = findViewById(R.id.question1Header);
+        textView2 = findViewById(R.id.question2Header);
 
         button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     string = editText.getText().toString();
                     toHash();
+                    toFnv();
                 } catch (NoSuchAlgorithmException e) {
                     e.printStackTrace();
                 }
@@ -41,11 +45,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void toFnv() {
+        FNV fnv = new FNV();
+        textView2.setText("FNV 64 bit"+fnv.fnv1a_64(string.getBytes()));
+    }
+
     private void toHash() throws NoSuchAlgorithmException {
         Log.v("EditText", string);
         MessageDigest msg = MessageDigest.getInstance("MD5");
         msg.update(string.getBytes(), 0, string.length());
         String digest1 = new BigInteger(1, msg.digest()).toString(16).substring(0, 16);
-        textView.setText("MD5 substring(0, 16) "+digest1);
+        textView1.setText("MD5 substring(0, 16) "+digest1);
+
     }
 }
