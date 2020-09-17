4c1f6436e1498cd229a2b93e435ed4e9d8dbf8fb
Your Name <you@example.com>
2020-09-17 22:14:49 +0700

    https://en.wikipedia.org/wiki/SipHash

--- app/src/main/java/com/example/prototype1/MainActivity.java
+++ app/src/main/java/com/example/prototype1/MainActivity.java
@@ -13,13 +13,15 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import io.whitfin.siphash.SipHasher;
+
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     EditText editText;
     TextView textView1;
     TextView textView2;
-
+    TextView textView3;
     String string;
 
     @Override
@@ -30,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.answer1);
         textView1 = findViewById(R.id.question1Header);
         textView2 = findViewById(R.id.question2Header);
+        textView3 = findViewById(R.id.question3Header);
+
+        textView1.setTextIsSelectable(true);
+        textView2.setTextIsSelectable(true);
+        textView3.setTextIsSelectable(true);
 
         button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                     string = editText.getText().toString();
                     toHash();
                     toFnv();
+                    toSipHash();
                 } catch (NoSuchAlgorithmException e) {
                     e.printStackTrace();
                 }
@@ -45,9 +53,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void toSipHash() {
+        long sipHasher = SipHasher.hash("0123456789ABCDEF".getBytes(), string.getBytes(), 2, 4);
+        textView3.setText("SipHasher " + sipHasher);
+    }
+
     private void toFnv() {
         FNV fnv = new FNV();
-        textView2.setText("FNV 64 bit"+fnv.fnv1a_64(string.getBytes()));
+        textView2.setText("FNV 64 bit " + fnv.fnv1a_64(string.getBytes()));
     }
 
     private void toHash() throws NoSuchAlgorithmException {
@@ -55,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         MessageDigest msg = MessageDigest.getInstance("MD5");
         msg.update(string.getBytes(), 0, string.length());
         String digest1 = new BigInteger(1, msg.digest()).toString(16).substring(0, 16);
-        textView1.setText("MD5 substring(0, 16) "+digest1);
+        textView1.setText("MD5 substring(0, 16) " + digest1);
 
     }
 }
